ba346c2c913564c36bd2916011d55c4d43209ec2
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-23 06:07:16 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+/* primitive and non primitive data data */
+// primitive
+// 7 category : String ,Number,Boolean,null,undefined,Symbol,BigInt
+ const id=Symbol('123')
+ const anotherId=Symbol('123')
+const bignumber=382868567826858525882n  
+// console.log(id===anotherId)
+// console.log(id==anotherId)
+// reference (non primitive)
+// array,objects,functions
+const heroes=["saktiman","naagraj","doga"];
+
+let mydetail={
+    name:"prajjawal kumar singh",
+    age:18
+}
+let myfunction=function(){
+  console.log("hello world");
+}
+
