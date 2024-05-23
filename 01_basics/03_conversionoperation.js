8f16f5989b36d182ae8c18c1c881bd966a4e98fe
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-23 05:25:25 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,12 @@
+let score ="33"
+console.log(typeof(score));
+let value=Number(score);
+console.log(typeof value);
+//"33"=>33
+// "33abc"=>NaN
+let isloggedIn=1;
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true 
+// ""=>false
+// "anystring"=>true
