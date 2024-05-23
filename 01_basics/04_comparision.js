59d3c68750ee6654cdd56f45a67ef17f0918bf3f
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-23 05:51:39 +0000

    comparion

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+/*
+false 
+false 
+true
+*/
+// console.log(undefined>0)
+// console.log(undefined==0);
+// console.log(undefined>=0);
+/*
+false
+false 
+false
+*/
+// === (datatype and value both will check)
+
+console.log("2"===2)
