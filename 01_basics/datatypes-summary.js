8606a88fa337d06d1b14884af6491e35d9e962fd
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-23 06:23:20 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -18,3 +18,17 @@ let myfunction=function(){
   console.log("hello world");
 }
 
+// stack(Primitive),heap(Non-Primitive)
+let myname="prajjawlsingh"
+let anothername=myname;
+anothername="srikant"
+console.table([myname,anothername])
+
+let userone={
+    email: "user@google.com",
+    upid:"user@ybl"
+}
+let usertwo=userone
+usertwo.email="jkkkkkk"
+console.table([userone,usertwo]);
+
