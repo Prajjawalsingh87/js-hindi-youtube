fdc0b214a43fcef8058b558d25c3bd0f86bf8e68
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-23 05:38:08 +0000

    operator

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,12 +1,33 @@
-let score ="33"
-console.log(typeof(score));
-let value=Number(score);
-console.log(typeof value);
-//"33"=>33
-// "33abc"=>NaN
-let isloggedIn=1;
-let booleanIsLoggedIn=Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
+// let score ="33"
+// //console.log(typeof(score));
+// let value=Number(score);
+// //console.log(typeof value);
+// //"33"=>33
+// // "33abc"=>NaN
+// let isloggedIn=1;
+// let booleanIsLoggedIn=Boolean(isloggedIn)
+// //console.log(booleanIsLoggedIn);
 //1=>true 
 // ""=>false
 // "anystring"=>true
+// **************************** operations *******************
+
+// let value = 3;
+// let negvalue = -value
+// console.log(negvalue);
+// let str1 = "hello"
+// let str2 = " prajjawal";
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//console.log(+true);
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// let gamecounter=100
+// ++gamecounter;
+// console.log(gamecounter)
+
