7620682b3ea09bd8cb8f0b4b1f46e0be1f49b1d3
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-24 04:40:32 +0000

    maths

---
+++ 01_basics/06_number_and_math.js
@@ -0,0 +1,12 @@
+// const score=400
+// console.log(score);
+// const balance=new Number(1000)
+// console.log(balance)
+//*********************MATHS *********/
+// console.log(Math);
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+console.log(Math.round(((Math.random()*10)+1),1));
+ const min=10;
+ const max=20;
+ console.log(Math.floor(Math.random()*(max-min+1)+min))
