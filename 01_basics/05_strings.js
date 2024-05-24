ba276ccc45c66dbfb1f1dc473ac8406386d1adfb
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-24 04:05:15 +0000

    string function

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const name ="prajjawal"
+const repoCount=50
+//console.log(name+repoCount+'value');
+// console.log(`hello my name is ${name } and my repo count is ${repoCount}`);
+const gamename=new String('Prajj-awal')
+//console.log(gamename.length)
+//console.log(gamename.toUpperCase());
+//console.log(gamename.charAt(2))
+//console.log(gamename.indexOf('a'));
+// const newstring=gamename.substring(0,4);
+// console.log((newstring));
+
+// const anotherstring =gamename.slice(2,4);
+// console.log(anotherstring);
+// const newstring="   prajjawalkumarsingh   "
+// console.log(newstring);
+// console.log(newstring.trim());
+// const url ="https://prajjawal.com//praks%20";
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('prajjawal'));
+
+console.log(gamename.split('-'));
