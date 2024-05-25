3b36b5af061a3e77661211289fd3562bf3e41a44
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-25 09:21:21 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,5 +7,50 @@ if (1) {
 }
 //console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
+
+
+
+// nested scope 
+function one(){
+    const username="prajjawal"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    //console.log(website);
+    //two()
+}
+one()
+if(true){
+    const username="pks"
+    if(username=="pks"){
+        const website=" codeforces"
+        //console.log(username+website)
+    }
+    //console.log(website)
+}
+//console.log(username)
+//*************************************************************interesting *****************************************/
+// function addone(num){
+//     return num+1;
+// }
+// addone(5)
+console.log(addone(5));
+function addone(num){
+    return num+1;
+}
+console.log(addtwo(6));// here it act as varibles and we cannt get varible before assign any value 
+let addtwo=function(num){
+    return num+2;
+}
+
+
+
+
+
+
+
+
+
 
