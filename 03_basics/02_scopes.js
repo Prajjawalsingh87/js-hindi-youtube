bd9b691fd706239500df7353bb42b7444cbb7284
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-25 09:06:09 +0000

    scope disscuss

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,11 @@
+
+{ }// scope  it come when conditional statement came,loop,function like this only
+if (1) {
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+//console.log(a);
+//console.log(b);
+console.log(c);
+
