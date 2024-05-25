0442f5a2f595bb2f4d959f0f2dec550ad5b06cc8
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-25 08:58:17 +0000

    function detail

---
+++ 03_basics/01_function.js
@@ -0,0 +1,15 @@
+function calculateCartprice(val1,val2,...num1){
+    return num1
+}
+//console.log(calculateCartprice(200,400,500,700,900))
+const user={
+    username:"prajjawal",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`user name is ${anyobject.username} and price is ${anyobject.price}`)
+}
+handleObject({
+    username:"pks",
+    price :399
+});
