01e3db02b4a8dcdceb333fd52942767bb489caba
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-26 11:48:43 +0000

    control

---
+++ 04_controlflow/truely.js
@@ -0,0 +1,10 @@
+const obj={}
+if(Object.keys(obj).length==0){
+   console.log("lauda")
+}
+// Nullish coalescing operator
+let val1;
+val1=5??10
+val1=null??10??20
+//terniary operator
+condition?true:false;
