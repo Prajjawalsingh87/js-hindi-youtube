25e20321be132208a54e26e58eae5d0aefa46b6c
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-26 11:29:57 +0000

    control flow

---
+++  04_control_flow/one.js
@@ -0,0 +1,26 @@
+// if statement
+if(true){
+
+    // then only inside scode will excute otherwise else will execute
+}
+else{
+
+}
+// comparision 
+switch (key) {
+    case value:
+        
+        break;
+    case value:
+        
+        break;
+    case value:
+        
+        break;
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
