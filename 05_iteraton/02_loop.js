a3d072dbfcde2d33e4fa7874778995cc3c003605
Prajjawalsingh87 <146062103+Prajjawalsingh87@users.noreply.github.com>
2024-05-26 14:02:43 +0000

    loop

---
+++ 05_iteraton/02_loop.js
@@ -0,0 +1,9 @@
+// let indx=0;
+// while (indx<10) {
+//      console.log(`Value of index is ${indx}`);
+//      indx++;
+// }
+let score =11;
+// do {
+//      console.log(`score is ${score}`);
+// } while (score<=10);
